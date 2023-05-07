a1d115b47455251c67832d8167a934df3c4ed67b
soi013 <soi2flatline@gmail.com>
2023-05-07 15:57:38 +0900

    10。Turn the laser on and off with the trigger

--- Assets/_VRPathway/Assets/Scripts/Scanner.cs
+++ Assets/_VRPathway/Assets/Scripts/Scanner.cs
@@ -13,7 +13,7 @@ public class Scanner : XRGrabInteractable
     protected override void Awake()
     {
         base.Awake();
-        laserRenderer.gameObject.SetActive(false);
+        ChangeRaserEnable(false);
     }
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
@@ -31,5 +31,22 @@ public class Scanner : XRGrabInteractable
     private void ChangeOpen(bool isOpen)
     {
         animator.SetBool("Opened", isOpen);
+    }
+
+    protected override void OnActivated(ActivateEventArgs args)
+    {
+        base.OnActivated(args);
+        ChangeRaserEnable(true);
+    }
+
+    protected override void OnDeactivated(DeactivateEventArgs args)
+    {
+        base.OnDeactivated(args);
+        ChangeRaserEnable(false);
+    }
+
+    private void ChangeRaserEnable(bool isOn)
+    {
+        laserRenderer.gameObject.SetActive(isOn);
     }
 }
