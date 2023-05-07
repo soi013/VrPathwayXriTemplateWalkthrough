5cfae4f69fcff137f7fcb970bb8844c14e187713
soi013 <soi2flatline@gmail.com>
2023-05-07 14:55:47 +0900

    3。Extend the basic grab functionality

---
+++ Assets/_VRPathway/Assets/Scripts/Scanner.cs
@@ -0,0 +1,16 @@
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Scanner : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
