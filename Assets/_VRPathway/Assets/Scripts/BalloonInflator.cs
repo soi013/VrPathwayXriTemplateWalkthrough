76b5c9a60d49fa9386560349ba7e9a08f9b16368
soi013 <soi2flatline@gmail.com>
2023-05-07 19:55:06 +0900

    6。Scale the balloon based on the trigger value

--- Assets/_VRPathway/Assets/Scripts/BalloonInflator.cs
+++ Assets/_VRPathway/Assets/Scripts/BalloonInflator.cs
@@ -7,16 +7,41 @@ public class BalloonInflator : XRGrabInteractable
     [Header("Balloon Data")]
     [SerializeField]
     private Transform attachPoint;
+
     [SerializeField]
     private Balloon balloonPrefab;
 
     private Balloon balloonInstance;
 
+    private XRBaseController grabedController;
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
+
+        if (args.interactorObject is not XRBaseControllerInteractor controllerInteractor)
+            return;
+
+        grabedController = controllerInteractor.xrController;
+
         balloonInstance = Instantiate(balloonPrefab, attachPoint);
+
+
+        //var value = controller.val.value;
+        //balloonInstance.transform.localScale = Vector3.one * value;
+
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+
+        if (!isSelected || grabedController == null)
+            return;
+
+        float valueGrab = grabedController.activateInteractionState.value;
+
+        balloonInstance.transform.localScale = Vector3.one * Mathf.Lerp(1.0f, 4.0f, valueGrab);
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)
